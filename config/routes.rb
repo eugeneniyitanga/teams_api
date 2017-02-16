330f5b38dc5bf4870693318eae922086530622e8
Eugene Niyitanga <eugene.niyitanga2015@gmail.com>
2017-02-16 13:39:44 +0200

    created a new team_api and added resful actions

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  
+  get "/teams", to: 'teams#index'
+  post"/teams", to: 'teams#create'
+  get "/teams/:id", to:'teams#show'
+  patch "/teams/:id", to:'teams#update'
+  delete "/teams/:id", to:'teams#destroy'
+
+namespace :api do
+  namespace :v2 do
+    get "/teams", to: 'teams#index'
+    post"/teams", to: 'teams#create'
+    get "/teams/:id", to:'teams#show'
+    patch "/teams/:id", to:'teams#update'
+    delete "/teams/:id", to:'teams#destroy' 
+  end 
+end 
+end
---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+100.times do 
+  Team.create(
+    creature: Faker::Team.creature,
+    name: Faker::Team.name, 
+    state: Faker::Team.state,
+    sport: Faker::Team.sport
+  )
+end 
